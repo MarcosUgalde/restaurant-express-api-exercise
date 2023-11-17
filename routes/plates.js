b5da1081165e2955cd14c8ff733e21da6a340c85
Marcos <marcosugaldemartinez@gmail.com>
2023-11-17 21:21:48 +0100

    feat: creado segundo endpoint POST

--- routes/plates.js
+++ routes/plates.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const fs = require("fs");
 
 const plates = require("../data/menu.json");
+
 router.get("/", (req, res) => {
   res.send(plates);
 });
+
+router.post("/", (req, res) => {
+  const newPlate = [...plates, req.body];
+  fs.writeFile("./data/menu.json", JSON.stringify(newPlate), (err) => {
+    if (err) throw err;
+    res.send("Plate Created");
+  });
+});
+
 module.exports = router;
